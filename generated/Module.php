dagger/dagger

User defaults: persist any dagger function argument to .env
Signed-off-by: Solomon Hykes <solomon@dagger.io>

sdk/php/generated/Module.php
@@ -68,6 +68,15 @@ class Module extends Client\AbstractObject implements Client\IdAble
     }
 
     /**
+     * User-defined defaults values, loaded from local .env files.
+     */
+    public function localDefaults(): EnvFile
+    {
+        $innerQueryBuilder = new \Dagger\Client\QueryBuilder('localDefaults');
+        return new \Dagger\EnvFile($this->client, $this->queryBuilderChain->chain($innerQueryBuilder));
+    }
+
+    /**
      * The name of the module
      */
     public function name(): string

sdk/php/generated/ModuleSource.php
@@ -177,6 +177,15 @@ class ModuleSource extends Client\AbstractObject implements Client\IdAble
     }
 
     /**
+     * User-defined defaults read from local .env files
+     */
+    public function localDefaults(): EnvFile
+    {
+        $innerQueryBuilder = new \Dagger\Client\QueryBuilder('localDefaults');
+        return new \Dagger\EnvFile($this->client, $this->queryBuilderChain->chain($innerQueryBuilder));
+    }
+
+    /**
      * The name of the module, including any setting via the withName API.
      */
     public function moduleName(): string

dagger/dagger-php-sdk

generated/Module.php
@@ -136,6 +136,15 @@ class Module extends Client\AbstractObject implements Client\IdAble
     }
 
     /**
+     * User-defined default values, loaded from local .env files.
+     */
+    public function userDefaults(): EnvFile
+    {
+        $innerQueryBuilder = new \Dagger\Client\QueryBuilder('userDefaults');
+        return new \Dagger\EnvFile($this->client, $this->queryBuilderChain->chain($innerQueryBuilder));
+    }
+
+    /**
      * Retrieves the module with the given description
      */
     public function withDescription(string $description): Module

generated/ModuleSource.php
@@ -258,6 +258,15 @@ class ModuleSource extends Client\AbstractObject implements Client\IdAble
     }
 
     /**
+     * User-defined defaults read from local .env files
+     */
+    public function userDefaults(): EnvFile
+    {
+        $innerQueryBuilder = new \Dagger\Client\QueryBuilder('userDefaults');
+        return new \Dagger\EnvFile($this->client, $this->queryBuilderChain->chain($innerQueryBuilder));
+    }
+
+    /**
      * The specified version of the git repo this source points to.
      */
     public function version(): string